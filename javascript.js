22b87ad3902c6c61ff8ab06a179308ca1f33574f
rafaelbritoo <rafael.britoo@gmail.com>
2019-12-02 19:26:51 -0300

    Desenvolvimento parte visual da arvore

--- javascript.js
+++ javascript.js
@@ -1,90 +1,77 @@
 window.onload = () => {
-    // define the tree-item component
-    // Vue.component('tree-item', {
-    //     template: '#item-template',
-    //     props: {
-    //         item: Object
-    //     },
-    //     data: function () {
-    //         return {
-    //             isOpen: false
-    //         }
-    //     },
-    //     computed: {
-    //         isFolder: function () {
-    //             return this.item.sub_itens &&
-    //                 this.item.sub_itens.length
-    //         }
-    //     },
-    //     methods: {
-    //         toggle: function () {
-    //             if (this.isFolder) {
-    //                 this.isOpen = !this.isOpen
-    //             }
-    //         }
-    //     }
-    // })
-
+    Vue.component('tree-item', {
+        template: '#item-template',
+        props: {
+            item: Object
+        },
+        data: function () {
+            return {
+                isOpen: false
+            }
+        },
+        computed: {
+            isFolder: function () {
+                return this.item.filhos &&
+                    this.item.filhos.length
+            }
+        },
+        methods: {
+            toggle: function () {
+                if (this.isFolder) {
+                    this.isOpen = !this.isOpen
+                }
+            }
+        }
+    })
 
     new Vue({
         el: '#app',
         data() {
             return {
-                selected: null,
-                orgaos: [
-
-                ],
-                bancas: [
-
-                ]
+                form: {
+                    orgao: null,
+                    banca: null,
+                },
+                orgaos: [],
+                bancas: [],
+                arvore: [],
+                show:true
+            }
+        },
+        
+        mounted() {
+            axios
+                .get('http://localhost/api/orgao/')
+                .then(response => (this.setDadosCombo(response.data, "orgaos")));
+        
+            axios
+                .get('http://localhost/api/banca/')
+                .then(response => (this.setDadosCombo(response.data, "bancas")));
+        },
+        methods: {
+            setDadosCombo(data, propriedade) {
+                this[propriedade] = this.setComboDados(data, "id", "nome");
+            },
+            setComboDados(dados, campoValue, campoTexto) {
+                let listaCombo = [];
+        
+                dados.forEach((item) => {
+                    listaCombo.push(
+                        {
+                            value: item[campoValue],
+                            text: item[campoTexto]
+                        }
+                    );
+                });
+        
+                return listaCombo;
+            },
+            onSubmit(evt) {
+                evt.preventDefault()
+                axios
+                    .get('http://localhost/api/arvores-assuntos/orgao/'+ this.form.orgao + '/banca/' + this.form.banca)
+                    .then(response => this.arvore = response.data.data);
             }
         }
-        // data() {
-        //     return {
-        //         form: {
-        //             orgao: null,
-        //             banca: null,
-        //         },
-        //         bancas: [],
-        //         orgaos: [],
-        //         arvoreProgramaEstudo: [],
-        //         show: true
-        //     }
-        // },
-        // mounted() {
-        //     axios
-        //         .get('http://symfony.localhost/api/orgao/')
-        //         .then(response => (this.setDadosComboDoResponse(response.data, "orgaos")));
-        //
-        //     axios
-        //         .get('http://symfony.localhost/api/banca/')
-        //         .then(response => (this.setDadosComboDoResponse(response.data, "bancas")));
-        // },
-        // methods: {
-        //     setDadosComboDoResponse(data, nomePropriedade) {
-        //         this[nomePropriedade] = this.setDadosParaCombo(data, "id", "nome");
-        //     },
-        //     setDadosParaCombo(dados, campoValue, campoTexto) {
-        //         let listaCombo = [];
-        //
-        //         dados.forEach((item) => {
-        //             listaCombo.push(
-        //                 {
-        //                     value: item[campoValue],
-        //                     text: item[campoTexto]
-        //                 }
-        //             );
-        //         });
-        //
-        //         return listaCombo;
-        //     },
-        //     onSubmit(evt) {
-        //         evt.preventDefault()
-        //         axios
-        //             .get('http://symfony.localhost/api/programa_estudo/',
-        //                 {params: this.form})
-        //             .then(response => this.arvoreProgramaEstudo = response.data);
-        //     }
-        // }
     })
 };
